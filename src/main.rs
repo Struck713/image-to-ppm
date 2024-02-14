eb60cf1c90b4fc8c7900d5e2b80de0a876b71172
Noah Struck <noahstruck71303@gmail.com>
2024-02-13 20:20:26 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::{fs::File, io::Write};
+
+use clap::{Parser};
+use image::io::Reader;
+
+#[derive(Parser)]
+struct Cli {
+    /// The image you want to convert to PPM (supported formats: .png)
+    input: String,
+    /// The output file name and path
+    output: String,
+}
+
+fn main() {
+    let args = Cli::parse();
+    
+    let result = Reader::open(args.input);
+    match result {
+        Ok(reader) => {
+            if let Ok(img) = reader.decode() {
+                let bytes = img.as_bytes();
+                write_ppm_image(args.output, img.width(), img.height(), bytes);
+            }
+        },
+        Err(err) => {
+            println!("error: {}", err.to_string())
+        }
+    }
+
+}
+
+fn write_ppm_image(path: String, width: u32, height: u32, bytes: &[u8]) {
+    println!("Image is {} by {}", width, height);
+    if let Ok(mut file) = File::create(&path) {
+        
+        let mut data = String::new();
+
+        // metadata
+        data.push_str("P6");
+        data.push(' ');
+        data.push_str(width.to_string().as_str());
+        data.push(' ');
+        data.push_str(height.to_string().as_str());
+        data.push(' ');
+
+        let largest_pixel_value = bytes.into_iter().max().unwrap().to_string();
+        data.push_str(&largest_pixel_value);
+
+        data.push('\n');
+        file.write(data.as_bytes()).expect("Failed to write metadata to the file");
+
+        
+        // write
+        let mut pixels_written = 0;
+        let mut index = 0;
+        while index < bytes.len() {
+            if index != 0 && index % 4 == 0 { // skip alpha byte
+                file.write(&bytes[index - 4..index - 1]).expect("Failed to write pixels to file");
+                pixels_written += 1;
+            }
+            index += 1;
+        }
+
+        // write final pixel
+        file.write(&bytes[index - 4..index - 1]).expect("Failed to write pixels to file");
+        pixels_written += 1;
+
+        println!("{} pixels written to {:?}", pixels_written, &path);
+    }
+}
