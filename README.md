f5acfdc7c0a03de99c7321adf79c2477eafcac2d
Struck713 <noahstruck71303@gmail.com>
2024-02-14 03:38:30 -0500

    Condensed a lot of code, prettified error messages and added jpg support

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # image-to-ppm
 
-[PPM](https://netpbm.sourceforge.net/doc/ppm.html) is like the WAV file of images. It's just raw pixel data. So I wrote this Rust program to make images worse by converting them to PPM. At this point, it can only do PNG but at some point I will *probably* implement more image formats.
+[PPM](https://en.wikipedia.org/wiki/Netpbm) is like the [WAV](https://en.wikipedia.org/wiki/WAV) file of images. It's just raw pixel data. 
+So I wrote this Rust program to make images worse by converting them to PPM.
+
+Currently, this program can convert JPG and PNG images to PPM.
+
+To use it simply run: `image-to-ppm <input file> <output file>`
+
+There's not much more to it.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{fs::File, io::Write};
 
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use clap::error::{ErrorKind};
+
 use image::io::Reader;
 
 #[derive(Parser)]
 struct Cli {
-    /// The image you want to convert to PPM (supported formats: .png)
+    /// The image you want to convert to PPM (supported formats: .png, .jpg)
     input: String,
     /// The output file name and path
     output: String,
@@ -13,67 +15,65 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    
-    let result = Reader::open(args.input);
+    let mut command = Cli::command();
+
+    if !args.output.ends_with(".ppm") {
+        let error = command.error(ErrorKind::Io, "Please provide a valid output file name with the .ppm extension");
+        error.exit();
+    }
+
+    let result = Reader::open(&args.input);
     match result {
         Ok(reader) => {
             if let Ok(img) = reader.decode() {
                 let bytes = img.as_bytes();
-                write_ppm_image(args.output, img.width(), img.height(), bytes);
-            }
-        },
-        Err(err) => {
-            println!("error: {}", err.to_string())
-        }
-    }
+                let width = img.width();
+                let height = img.height();
+                
+                println!("{}: {} by {}", &args.input, width, height);
+                println!("Bytes to write: {}", bytes.len());
+                if let Ok(mut file) = File::create(&args.output) {
+                    let mut data = String::new();
+                    
+                    // metadata
+                    data.push_str("P6");
+                    data.push(' ');
+                    data.push_str(width.to_string().as_str());
+                    data.push(' ');
+                    data.push_str(height.to_string().as_str());
+                    data.push(' ');
 
-}
+                    let largest_pixel_value = bytes.into_iter().max().unwrap().to_string();
+                    data.push_str(&largest_pixel_value);
+                    data.push('\n');
 
-fn write_ppm_image(path: String, width: u32, height: u32, bytes: &[u8]) {
-    println!("Image is {} by {}", width, height);
-    if let Ok(mut file) = File::create(&path) {
-        
-        let mut data = String::new();
+                    file.write(data.as_bytes())
+                        .expect("Failed to write metadata to the file");
+                    
+                    let alpha_layer = img.color().has_alpha();
+                    println!("Image has alpha layer: {}", if alpha_layer { "yes" } else { "no" });
 
-        // metadata
-        data.push_str("P6");
-        data.push(' ');
-        data.push_str(width.to_string().as_str());
-        data.push(' ');
-        data.push_str(height.to_string().as_str());
-        data.push(' ');
+                    let pixels_written;
+                    if alpha_layer {
+                        let mut index = 0;
+                        while index < bytes.len() {
+                            index += 4; // skip alpha layer
+                            file.write(&bytes[index - 4..index - 1])
+                                .expect("Failed to write pixels to file");
+                        }
+                        pixels_written = bytes.len() / 4;
+                    } else {
+                        file.write(bytes).expect("Failed to write pixels to file");
+                        pixels_written = bytes.len() / 3;
+                    }
 
-        let largest_pixel_value = bytes.into_iter().max().unwrap().to_string();
-        data.push_str(&largest_pixel_value);
-
-        data.push('\n');
-        file.write(data.as_bytes()).expect("Failed to write metadata to the file");
-
-        let image_has_alpha = ((bytes.len() / 4) as u32) == width * height;
-        println!("Bytes to write: {}", bytes.len());
-        println!("Image has alpha layer: {}", image_has_alpha);
-
-        let mut pixels_written = 0;
-        if image_has_alpha {
-            // write
-            let mut index = 1;
-            while index < bytes.len() {
-                if index % 4 == 0 { // skip alpha byte
-                    file.write(&bytes[index - 4..index - 1]).expect("Failed to write pixels to file");
-                    pixels_written += 1;
+                    println!("{}: {} pixels written", &args.output, pixels_written);
                 }
-                index += 1;
             }
-
-            // write final pixel
-            file.write(&bytes[index - 4..index - 1]).expect("Failed to write pixels to file");
-            pixels_written += 1;
-        } else {
-            file.write(bytes).expect("Failed to write pixels to file");
-            pixels_written += bytes.len() / 3;
         }
-
-
-        println!("{} pixels written to {}", pixels_written, &path);
+        Err(err) => {
+            let error = command.error(ErrorKind::Io, err.to_string());
+            error.exit();
+        }
     }
 }
